fd9ede8ef8473ae6721c84c8792ca241c9ef0b28
zehreken <zehreken@gmail.com>
2020-01-10 22:54:05 +0100

    Add yellow shaking circles

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,17 @@
 use nannou::prelude::*;
+use rand;
 
 fn main() {
     nannou::app(model).run();
 }
 
 struct Model {
-    _window: WindowId,
+    random_seed: u32,
+    window: WindowId,
 }
 
 fn model(app: &App) -> Model {
-    let _window = app
+    let window = app
         .new_window()
         .with_dimensions(512, 512)
         .with_title("nannou")
@@ -17,7 +19,10 @@ fn model(app: &App) -> Model {
         .event(event) // The function that will be called when the window receives events.
         .build()
         .unwrap();
-    Model { _window }
+    Model {
+        random_seed: 1,
+        window: window,
+    }
 }
 
 fn event(_app: &App, _model: &mut Model, event: WindowEvent) {
@@ -30,24 +35,24 @@ fn view(app: &App, _model: &Model, frame: &Frame) {
     let t = app.time;
     let draw = app.draw();
 
-    draw.background().color(PLUM);
+    draw.background().color(BLACK);
 
-    for i in 0..9 {
-        let radius = 450.0 - i as f32 * 50.0;
-        let points = (0..=360).step_by(45).map(|i| {
-            let radian = deg_to_rad(i as f32);
-            let x = radian.sin() * radius;
-            let y = radian.cos() * radius;
-            pt2(x, y)
-        });
-        if i % 2 == 0 {
-            draw.polygon()
-                .color(STEELBLUE)
-                .points(points)
-                .rotate(-t)
-                .x_y(-25.0, -25.0);
-        } else {
-            draw.polygon().color(PLUM).points(points).rotate(t);
+    for i in 0..16 {
+        for j in 0..16 {
+            draw.ellipse()
+                .w_h(
+                    30.0 + 12.0 * t.cos() * rand::random::<f32>(),
+                    30.0 + 12.0 * t.cos() * rand::random::<f32>(),
+                )
+                .x_y(
+                    -256.0 + i as f32 * 32.0 + 16.0 + rand::random::<f32>(),
+                    -256.0 + j as f32 * 32.0 + 16.0 + rand::random::<f32>(),
+                )
+                .color(rgb(
+                    0.9 + rand::random::<f32>() * 0.1,
+                    0.7 + rand::random::<f32>() * 0.3,
+                    0.0,
+                ));
         }
     }
 
