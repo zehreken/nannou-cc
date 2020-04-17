7029f6812105374831b714d22c9d397a267149e8
zehreken <zehreken@gmail.com>
2020-04-17 23:19:41 +0200

    Update a5

--- src/a5/mod.rs
+++ src/a5/mod.rs
@@ -25,20 +25,27 @@ fn view(app: &App, _model: &Model, frame: Frame) {
 
     draw.background().color(BLACK);
 
-    let cycle = 8.0;
-    let wave: Vec<Point2> = (0..=360)
-        .step_by(2)
-        .map(|i| {
-            let angle = deg_to_rad(500.0 * t + cycle * i as f32);
-            let radian = deg_to_rad(i as f32);
-            let val = radian.sin() * 20.0 + angle.sin();
-            let x = radian.cos() * 20.0 + angle.cos();// * (-PI * cycle + cycle * i as f32 * 2.0 * PI as f32 / 360.0);
-            let scale = 10.0;
-            pt2(x * scale, val * scale)
-        })
-        .collect();
+    let colors = vec![YELLOW, RED, WHITE, ORANGE, CORNFLOWERBLUE];
 
-    draw.polyline().points(wave).color(YELLOW);
+    let cycle = 20.0;
+    for c in 1..=5 {
+        let wave: Vec<Point2> = (0..=360)
+            .map(|i| {
+                let angle = deg_to_rad(50.0 * t * c as f32 + cycle * i as f32);
+                let sine_y = angle.sin();
+                let radian = deg_to_rad(i as f32);
+                let val = radian.sin() * 20.0 + radian.sin() * sine_y * 2.0;
+                let x = radian.cos() * 20.0 + radian.cos() * sine_y * 2.0; // * (-PI * cycle + cycle * i as f32 * 2.0 * PI as f32 / 360.0);
+                let scale = 2.0 * c as f32;
+                pt2(x * scale, val * scale)
+            })
+            .collect();
+
+        draw.polyline()
+            .weight(5.0)
+            .points(wave)
+            .color(colors[c - 1]);
+    }
 
     draw.to_frame(app, &frame).unwrap();
 }
