f128e14aea8cef463e899b4a7d166acc9bed06be
zehreken <zehreken@gmail.com>
2020-04-26 22:36:47 +0200

    Update a7, add sine wave

--- src/a7/mod.rs
+++ src/a7/mod.rs
@@ -20,17 +20,34 @@ fn model(app: &App) -> Model {
 }
 
 fn view(app: &App, model: &Model, frame: Frame) {
+    let t = 44.0 * app.duration.since_start.as_secs_f32();
     let draw = app.draw();
     draw.background().color(BLACK);
 
     let points = (0..360).map(|i| {
-        let radian = deg_to_rad(20.0 * i as f32);
+        let radian = deg_to_rad(i as f32);
+        let x = radian.cos();
+        let y = radian.sin();
+        let scale = 100.0;
+        pt2(x * scale, y * scale)
+    });
+    draw.polyline().points(points).color(GREEN);
+
+    let radian = deg_to_rad(1.0 * t);
+    let scale = 100.0;
+    let points = vec![
+        pt2(0.0, 0.0),
+        pt2(scale * radian.cos(), scale * radian.sin()),
+    ];
+    draw.polyline().points(points).color(WHITE);
+
+    let points = (0..360).map(|i| {
+        let radian = deg_to_rad(t + i as f32);
         let y = radian.sin();
         let x = -1.0 + i as f32 / 180.0;
         let scale = 100.0;
         pt2(x * scale, y * scale)
     });
-
     draw.polyline().points(points).color(GOLD);
 
     draw.to_frame(app, &frame).unwrap();
