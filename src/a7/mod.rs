e21e34ef90be560b5c47514feb9cace99bca9799
zehreken <zehreken@gmail.com>
2020-04-26 23:58:33 +0200

    Improve a7

--- src/a7/mod.rs
+++ src/a7/mod.rs
@@ -20,7 +20,7 @@ fn model(app: &App) -> Model {
 }
 
 fn view(app: &App, model: &Model, frame: Frame) {
-    let t = 44.0 * app.duration.since_start.as_secs_f32();
+    let t = 4.0 * app.duration.since_start.as_secs_f32();
     let draw = app.draw();
     draw.background().color(BLACK);
 
@@ -35,18 +35,30 @@ fn view(app: &App, model: &Model, frame: Frame) {
 
     let radian = deg_to_rad(1.0 * t);
     let scale = 100.0;
-    let points = vec![
-        pt2(0.0, 0.0),
-        pt2(scale * radian.cos(), scale * radian.sin()),
-    ];
+    let (x, y) = (radian.cos(), radian.sin());
+    let points = vec![pt2(0.0, 0.0), pt2(scale * x, scale * y)];
+    draw.polyline().points(points).color(WHITE);
+
+    let radian = deg_to_rad(10.0 * t);
+    let (_x, _y) = (x + radian.cos() * 0.2, y + radian.sin() * 0.2);
+    let points = vec![pt2(scale * x, scale * y), pt2(scale * _x, scale * _y)];
+    draw.polyline().points(points).color(WHITE);
+
+    let radian = deg_to_rad(100.0 * t);
+    let (__x, __y) = (_x + radian.cos() * 0.2, _y + radian.sin() * 0.2);
+    let points = vec![pt2(scale * _x, scale * _y), pt2(scale * __x, scale * __y)];
     draw.polyline().points(points).color(WHITE);
 
     let points = (0..360).map(|i| {
-        let radian = deg_to_rad(t + i as f32);
+        let mut radian = deg_to_rad(t + i as f32);
         let y = radian.sin();
+        radian = deg_to_rad(10.0 * t + i as f32);
+        let _y = y + radian.sin() * 0.2;
+        radian = deg_to_rad(100.0 * t + i as f32);
+        let __y = _y + radian.sin() * 0.2;
         let x = -1.0 + i as f32 / 180.0;
         let scale = 100.0;
-        pt2(x * scale, y * scale)
+        pt2(x * scale, __y * scale)
     });
     draw.polyline().points(points).color(GOLD);
 
