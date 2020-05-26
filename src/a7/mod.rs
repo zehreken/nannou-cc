e61a1230da53531966b14671b79d96660cd3539e
zehreken <zehreken@gmail.com>
2020-05-26 23:59:37 +0200

    Improve a7

--- src/a7/mod.rs
+++ src/a7/mod.rs
@@ -1,6 +1,6 @@
 use nannou::prelude::*;
 
-const TIME_FACTOR: f32 = 3.0;
+const TIME_FACTOR: f32 = 20.0;
 pub fn start_a7() {
     nannou::app(model).update(update).run();
 }
@@ -22,12 +22,12 @@ fn model(app: &App) -> Model {
 }
 
 fn update(app: &App, model: &mut Model, _update: Update) {
-    let t = TIME_FACTOR * app.duration.since_start.as_secs_f32();
-    const SCALE: f32 = 50.0;
+    let t = TIME_FACTOR * app.duration.since_start.as_secs_f32() + 90.0;
+    const SCALE: f32 = 100.0;
     let mut point_on_circle = pt2(0.0, 0.0);
 
     for i in 0..5 {
-        let scale = SCALE as f32;
+        let scale = SCALE / (5 - i) as f32;
         let radian = deg_to_rad(2.0.pow(i as f32) as f32 * t);
         let (x, y) = (
             point_on_circle.x + radian.cos() * scale,
@@ -41,10 +41,11 @@ fn update(app: &App, model: &mut Model, _update: Update) {
 }
 
 fn view(app: &App, model: &Model, frame: Frame) {
-    let t = TIME_FACTOR * app.duration.since_start.as_secs_f32();
+    let t = TIME_FACTOR * app.duration.since_start.as_secs_f32() + 90.0;
     let draw = app.draw();
     draw.background().color(BLACK);
 
+    /*
     // The circle
     let points = (0..360).map(|i| {
         let radian = deg_to_rad(i as f32);
@@ -54,12 +55,13 @@ fn view(app: &App, model: &Model, frame: Frame) {
         pt2(x * scale, y * scale)
     });
     draw.polyline().points(points).color(GREEN);
+    */
 
-    const SCALE: f32 = 50.0;
+    const SCALE: f32 = 100.0;
     let mut center = pt2(0.0, 0.0);
     let mut point_on_circle = pt2(0.0, 0.0);
     for i in 0..5 {
-        let scale = SCALE as f32;
+        let scale = SCALE / (5 - i) as f32;
         let radian = deg_to_rad(2.0.pow(i as f32) as f32 * t);
         let (x, y) = (
             point_on_circle.x + radian.cos() * scale,
@@ -72,9 +74,7 @@ fn view(app: &App, model: &Model, frame: Frame) {
         center = point_on_circle;
     }
 
-    draw.polyline()
-        .points(model.points.clone())
-        .color(rgba(255u8, 155, 0, 50));
+    draw.polyline().points(model.points.clone()).color(GREEN);
 
     /*
     let scale = 100.0;
