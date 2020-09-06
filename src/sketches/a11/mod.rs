ae9cdaa122f4d457e4463a74280b14b50ea8e5f1
zehreken <zehreken@gmail.com>
2020-09-06 22:13:50 +0200

    Improve sketch a11

--- src/sketches/a11/mod.rs
+++ src/sketches/a11/mod.rs
@@ -1,7 +1,7 @@
 use super::sketch_utils::*;
 use nannou::prelude::*;
 
-const TIME_FACTOR: f32 = 10.0;
+const TIME_FACTOR: f32 = 20.0;
 pub fn start_a11() {
     nannou::app(model).update(update).run();
 }
@@ -37,6 +37,7 @@ fn model(app: &App) -> Model {
 
     const SEGMENT_COUNT: usize = 10;
     let segments = (0..SEGMENT_COUNT).map(|_| Segment::new()).collect();
+    app.set_loop_mode(LoopMode::loop_ntimes(720));
 
     Model {
         points: vec![],
@@ -52,13 +53,14 @@ fn key_pressed(app: &App, model: &mut Model, key: Key) {
 }
 
 fn update(app: &App, model: &mut Model, _update: Update) {
-    let t = TIME_FACTOR * app.duration.since_start.as_secs_f32() + 0.0;
-    const SCALE: f32 = 200.0;
+    let t = TIME_FACTOR * app.duration.since_start.as_secs_f32();
+    let t = app.elapsed_frames() as f32;
+    const SCALE: f32 = 100.0;
     let mut center = pt2(0.0, 0.0);
     let mut point_on_circle = pt2(0.0, 0.0);
 
     for i in 0..model.segments.len() {
-        let scale = SCALE / (model.segments.len() - i) as f32;
+        let scale = SCALE / (i + 1) as f32;
         let radian = PI / 2.0 + deg_to_rad(2.0.pow(i as f32) as f32 * t);
         let (x, y) = (
             point_on_circle.x + radian.cos() * scale,
@@ -81,6 +83,10 @@ fn view(app: &App, model: &Model, frame: Frame) {
         draw.background().color(BLACK);
     }
 
+    if app.elapsed_frames() == 720{
+        capture_frame(app, app.elapsed_frames());
+    }
+
     /*
     // The circle
     let points = (0..=360).map(|i| {
@@ -92,30 +98,42 @@ fn view(app: &App, model: &Model, frame: Frame) {
     });
     draw.polyline().points(points).color(CRIMSON);
     */
+    const ALPHA: u8 = 40;
     let colors = vec![
-        nannou::color::rgba(1.0, 0.0, 0.21, 0.2),
-        nannou::color::rgba(1.0, 0.2, 0.21, 0.2),
-        nannou::color::rgba(1.0, 0.4, 0.21, 0.2),
-        nannou::color::rgba(0.0, 0.0, 0.21, 0.2),
-        nannou::color::rgba(1.0, 1.0, 0.21, 0.2),
-        nannou::color::rgba(0.0, 0.8, 0.4, 0.2),
-        nannou::color::rgba(0.5, 0.0, 0.21, 0.2),
-        nannou::color::rgba(0.9, 0.3, 1.0, 0.2),
-        nannou::color::rgba(0.3, 0.3, 0.1, 0.2),
-        nannou::color::rgba(1.0, 0.8, 0.0, 0.2),
+        nannou::color::rgba8(255, 89, 94, ALPHA),
+        nannou::color::rgba8(255, 146, 76, ALPHA),
+        nannou::color::rgba8(255, 202, 58, ALPHA),
+        nannou::color::rgba8(197, 202, 48, ALPHA),
+        nannou::color::rgba8(138, 201, 38, ALPHA),
+        nannou::color::rgba8(54, 148, 157, ALPHA),
+        nannou::color::rgba8(25, 130, 196, ALPHA),
+        nannou::color::rgba8(66, 103, 172, ALPHA),
+        nannou::color::rgba8(86, 90, 160, ALPHA),
+        nannou::color::rgba8(106, 76, 147, ALPHA),
+        // nannou::color::rgba(1.0, 0.0, 0.21, 0.5),
+        // nannou::color::rgba(0.0, 0.6, 0.7, 0.4),
+        // nannou::color::rgba(1.0, 0.4, 0.21, 0.3),
+        // nannou::color::rgba(0.0, 0.0, 0.21, 0.2),
+        // nannou::color::rgba(0.0, 0.8, 0.4, 0.2),
+        // nannou::color::rgba(1.0, 1.0, 0.21, 0.2),
+        // nannou::color::rgba(0.5, 0.0, 0.21, 0.2),
+        // nannou::color::rgba(0.9, 0.3, 1.0, 0.2),
+        // nannou::color::rgba(0.3, 0.3, 0.1, 0.2),
+        // nannou::color::rgba(1.0, 0.8, 0.0, 0.2),
     ];
     for (i, segment) in model.segments.iter().enumerate() {
-        // let points = vec![segment.start, segment.end];
-        // draw.polyline()
-        //     .weight(1.0)
-        //     .points(points)
-        //     .color(WHITE);
+        let points = vec![segment.start, segment.end];
+        draw.polyline()
+            .weight(1.0 + 0.2 * i as f32)
+            .points(points)
+            .color(colors[i % colors.len()])
+            .y(-80.0);
 
         // if i == model.segments.len() - 1 {
-        draw.ellipse()
-            .w_h(30.0 - 3.0 * i as f32, 30.0 - 3.0 * i as f32)
-            .x_y(segment.end.x, segment.end.y)
-            .color(colors[i % colors.len()]);
+        // draw.ellipse()
+        //     .w_h(50.0 - 3.0 * i as f32, 50.0 - 3.0 * i as f32)
+        //     .x_y(segment.end.x, segment.end.y)
+        //     .color(colors[i % colors.len()]);
         // }
     }
 
